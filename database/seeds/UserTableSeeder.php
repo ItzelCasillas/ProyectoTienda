efe0a2030a19dbeeace0e027172563e0985e4625
valeriahily <valeria.riodel@hotmail.com>
2017-07-24 19:09:14 -0600

    Agregacion de usuarios de prueba

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,13 +11,28 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //Creando usuario para realizar perdido
-        $data = array{
-        	'name'     => 'Valeria Ahily',
-        	'email'    => 'valeria.riodel@hotmail.com',
-        	'password' => \Hash::make('holahola123'),
-        	'rol'      => 'admin',
+        //Creando usuario para realizar pedidos y administrar
+        $data = array(
+	        [
+				'name'       => 'Valeria Ahily',
+	        	'email'      => 'valeria.riodel@hotmail.com',
+	        	'password'   => \Hash::make('holahola123'),
+	        	'rol'        => 'admin',
+	        	'address'    => 'Infiernillo 666, Satanas',
+	        	'created_at' => new DateTime,
+	        	'updated_at' => new DateTime
+	        ],
 
-        }
+	        [
+	        	'name'       => 'Itzel Casillas',
+	        	'email'      => '1tz3lcg@gmail.com',
+	        	'password'   => \Hash::make('holahola123'),
+	        	'rol'        => 'user',
+	        	'address'    => 'Whataverygoodsoup, Tinguilingui',
+	        	'created_at' => new DateTime,
+	        	'updated_at' => new DateTime
+	        ],
+        );
+       	User::insert($data);
     }
 }

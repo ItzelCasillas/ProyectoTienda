25830395d9e95ed0889f017be64940330783b030
valeriahily <valeria.riodel@hotmail.com>
2017-07-24 20:22:41 -0600

    Agregacion de functions para categoria

--- app/Http/Controllers/tiendaController.php
+++ app/Http/Controllers/tiendaController.php
@@ -33,4 +33,41 @@ class tiendaController extends Controller
     	// return redirect('/');
 
     }
+
+    class encargadosController extends Controller
+{
+   public function registrarCat(){
+        $categories=Category::all();
+        return view('registrarCat', compact('categories'));
+   }
+
+   public function guardarCat(Requestd $datos){
+       $categories = new Category();
+         $categories->nombre=$datos->input('name');
+        $categories->save();
+        return redirect('/consultarCat');
+   }
+
+       public function eliminarCat($id){
+         $categories=Category::find($id);
+         $categories->delete();
+         return redirect('/consultarCat');
+   }
+
+   public function editarCat($id){
+         //$proyecto=Proyecto::find($id);
+         $categories=DB::table('categories')
+         ->where('categories.id', '=', $id)
+         ->first();
+         $categorie=Category::all();
+         return view('editarCat', compact('categories', 'categorie'));
+   }
+
+      public function actualizarCat($id, Request $datos){
+         $categories=Category::find($id);
+         $categories->name=$datos->input('name');
+         $categories->save();
+         return redirect('/consultarCat');
+   }
+
 }
